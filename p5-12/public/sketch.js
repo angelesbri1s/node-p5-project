1a9b7688c548b4f6b71f55be71252f8e08bddd44
angelesbri1s <angelesbriones@gmail.com>
2019-10-30 12:30:35 +0100

    added_files
    added client and server files

---
+++ p5-12/public/sketch.js
@@ -0,0 +1,41 @@
+function preload(){
+  // put preload code here
+}
+
+var socket;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+
+//for making connection with the server
+  socket = io();
+
+// in the setup i am definig if we have a new connect(
+  socket.on("mouseBroadcast", newDrawing);
+
+  function newDrawing(receivedData){
+    fill("yellow")
+    ellipse(receivedData.x, receivedData.y, 10);
+  }
+
+  background("red");
+
+
+
+}
+
+function draw() {
+  //ellipse(mouseX,mouseY,30)
+}
+
+function mouseDragged() {
+  fill("white");
+  ellipse(mouseX,mouseY,20)
+
+  var sendData = {
+    x:mouseX,
+    y:mouseY
+  }
+
+  socket.emit("mouse", sendData);
+}
